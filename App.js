d7cb2bd6e31cecd8597836d8b775afe19b1e18bf
AnasMaiza01 <anas.maiza03@gmail.com>
2024-09-20 00:02:55 +0100

    final

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { TouchableOpacity, StyleSheet, Text, TextInput, View } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, TextInput, View, ScrollView } from 'react-native';
 
 export default function App() {
   const [task,setTask] = React.useState("");
+  const [tasks, setTasks] = React.useState([]);
+  function add() {
+    setTasks([task, ...tasks]);
+    setTask("");
+  }
+  function deleteTask(index) {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  }
   return (
     <View style={styles.container}>
       <View style={styles.main}>
@@ -12,11 +22,21 @@ export default function App() {
           style={styles.inputStyle}
           placeholder='Enter a task'
           value={task}
-          onChange={setTask}
+          onChangeText={setTask}
         />
-        <TouchableOpacity style={styles.addTaskButton}>
+        <TouchableOpacity style={styles.addTaskButton} onPress={() => add()}>
           <Text style={styles.buttonText}>+ Add</Text>
         </TouchableOpacity>
+        <ScrollView style={styles.tasksContainer}>
+          {tasks.map((item, index) => (
+            <View key={index} style={styles.singleTaskContainer}>
+              <Text style={styles.task}>{item}</Text>
+              <TouchableOpacity style={styles.deleteButton} onPress={(index) => deleteTask(index)}>
+                <Text style={styles.deleteText}>X</Text>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </ScrollView>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -35,6 +55,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '80%',
     padding: 20,
+    paddingBottom: 0,
     paddingTop: 50,
     borderTopLeftRadius: 50,
     borderTopRightRadius: 50,
@@ -67,5 +88,45 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontWeight: 'bold',
     fontSize: 18,
+  },
+  tasksContainer: {
+    marginTop: 50,
+    width: "100%",
+    height: '80%',
+    padding: 20,
+  },
+  singleTaskContainer: {
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 50,
+    minHeight: 50,
+    marginBottom: 20,
+    flex: 1,
+    paddingHorizontal: 20,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+    overflow: 'hidden'
+  },
+  task: {    
+    paddingRight: 15,
+    color: '#000',
+    fontSize: 15,
+    flex: 1,
+    flexWrap: 'wrap'
+  },
+  deleteButton: {
+    backgroundColor: 'black',
+    height: '100%',
+    width: 50,
+    marginRight: -20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  deleteText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center'
   }
 });
